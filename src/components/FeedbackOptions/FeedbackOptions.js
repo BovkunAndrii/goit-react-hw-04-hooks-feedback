d3388dd4ddb737ddb6d947c7f839ec5b48b24b74
unknown <999barbell@gmail.com>
2021-08-18 21:52:05 +0300

    done-hw4

---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './FeedbackOptions.module.css';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ul className={styles.list}>
+      {Object.keys(options).map(key => (
+        <button className={styles.button} key={key} onClick={onLeaveFeedback}>
+          {key}
+        </button>
+      ))}
+    </ul>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.object.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
---
+++ src/components/SectionTitle/SectionTitle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './SectionTitle.module.css';
+
+const SectionTitle = ({ children }) => (
+  <div className={styles.section}>
+    <h2 className={styles.title}>Please leave feedback</h2>
+    {children}
+  </div>
+);
+
+export default SectionTitle;
